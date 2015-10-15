9dd8cec191bcea74f3788dd01a5eec91da0e2915
Nick Sypteras <nsypteras@gmail.com>
2015-10-15 18:43:06 -0400

    added bootstrap for grid system, sponsor images

--- public/src/js/controllers/homeController.js
+++ public/src/js/controllers/homeController.js
@@ -77,6 +77,53 @@
           }
         ];
 
+        var imgRoot = "/public/assets/img/sponsors/";
+
+        $scope.sponsors = [
+          {
+            name: "Alt Assets",
+            img: imgRoot + "altassets.jpg"
+          },
+          {
+            name: "D'Amore-McKim School of Business",
+            img: imgRoot + "dmsob-logo-color.png"
+          },
+          {
+            name: "John Hancock",
+            img: imgRoot + "john-hancock-color.png"
+          },
+
+          {
+            name: "Lake Partners",
+            img: imgRoot + "lake-partners.jpg"
+          },
+          {
+            name: "LSE SU Alternative Investments Conference",
+            img: imgRoot + "lse-aic-color.png"
+          },
+          {
+            name: "MFS",
+            img: imgRoot + "mfs-color.png"
+          },
+          {
+            name: "Northeastern Alumni Development Association",
+            img: imgRoot + "nuada-color.png"
+          },
+          {
+            name: "Northeastern Finance and Investment Club",
+            img: imgRoot + "nufic-color.png"
+          },
+
+          {
+            name: "State Street",
+            img: imgRoot + "statest-color.png"
+          },
+          {
+            name: "Wall Street Oasis",
+            img: imgRoot + "WSO-logo.jpg"
+          }
+        ];
+
     }
   ]);
 
