6ac4aa19660e2376a236e837e9eabf1ccf7e44c7
Nick Sypteras <nsypteras@gmail.com>
2015-11-12 00:00:46 -0500

    Rohan changes 20151011

--- public/src/js/controllers/homeController.js
+++ public/src/js/controllers/homeController.js
@@ -8,15 +8,17 @@
   app.controller('HomeController', [
     '$scope',
     '$http',
+    '$sce',
     '$window',
     '$document',
-    function($scope, $http, $window, $document) {
+    function($scope, $http, $sce, $window, $document) {
       console.log("compiled");
       // dev:: welcome message
       $scope.welcome = "Welcome home";
 
-      $scope.menuItems = ['About', 'Speakers', 'FAQ', 'Team',
-        /*'Sponsors',*/ 'Contact', 'Last'];
+      $scope.menuItems = ['About', 'Speakers', 'FAQ',
+      'Selected Presentations', 'Management Committee',
+      'Advisory Board', /*'Sponsors',*/ 'Contact', 'Last'];
 
         $scope.nav = {};
         $scope.nav.img = "/public/assets/img/nav-logo.png";
@@ -37,6 +39,18 @@
           // $scope.$apply();
 
           var elName = section.toLowerCase();
+
+          switch (elName) {
+            case 'management committee':
+              elName = 'team';
+              break;
+            case 'selected presentations':
+              elName = 'presentations';
+              break;
+            case 'advisory board':
+              elName = 'advisory-board';
+              break;
+          }
 
           var offset = 30;
           var duration = 2000;
@@ -151,6 +165,21 @@
           }
         ];
 
+        /*
+         * YouTube vids of selected presentations
+         */
+
+        $scope.presentations = [
+          {
+            url: $sce.trustAsResourceUrl("https://www.youtube.com/embed/byoSuIUhjsg"),
+            description: ""
+          },
+          {
+            url: $sce.trustAsResourceUrl("https://www.youtube.com/embed/hdFzfXmDoZE"),
+            description: ""
+          }
+        ];
+
         $scope.team = [
           {
             name: "Rohan Venkatesh",
@@ -193,6 +222,57 @@
             linkedin: "https://www.linkedin.com/pub/sam-marley/a4/a51/275"
           }
         ];
+
+        /*
+         * Advisory Board
+         */
+         $scope.advisoryBoard = [
+           {
+             name: "Nicholas Lara",
+             img: "/public/assets/img/advisory/lara.jpg",
+             linkedin: "https://www.linkedin.com/pub/nicholas-f-lara/2a/ba7/47b"
+           },
+           {
+             name: "Stephen Price",
+             img: "/public/assets/img/advisory/price.jpg",
+             linkedin: "https://www.linkedin.com/in/stephenprice93"
+           },
+           {
+             name: "Rohit Malrani",
+             img: "/public/assets/img/advisory/rohit.jpg",
+             linkedin: "https://www.linkedin.com/in/rohitmalrani"
+           },
+           {
+             name: "Amanda White",
+             img: "/public/assets/img/advisory/white.jpg",
+             linkedin: "https://www.linkedin.com/pub/amanda-white/27/86b/20a"
+           },
+           {
+             name: "Jay Hastings",
+             img: "/public/assets/img/advisory/hastings.jpg",
+             linkedin: "https://www.linkedin.com/pub/jayhastings"
+           },
+           {
+             name: "Michael DeCenzo",
+             img: "/public/assets/img/advisory/deCenzo.jpg",
+             linkedin: "https://www.linkedin.com/pub/michael-decenzo/13/873/46a"
+           },
+           {
+             name: "Daniel Asulin",
+             img: "/public/assets/img/advisory/asulin.jpg",
+             linkedin: "https://www.linkedin.com/in/danielgasulin"
+           },
+           {
+             name: "Rohan Venkatesh",
+             img: "/public/assets/img/advisory/rohanvenkatesh.jpg",
+             linkedin: "https://www.linkedin.com/in/rohanvenkatesh"
+           },
+           {
+             name: "Jake Fulton",
+             img: "/public/assets/img/advisory/jakefulton.jpg",
+             linkedin: "https://www.linkedin.com/pub/jake-fulton/43/a29/641"
+           }
+         ];
 
         var imgRoot = "/public/assets/img/sponsors/";
 
@@ -271,8 +351,12 @@
         $scope.anSpeakerPics = false;
         $scope.anFaq = false;
         $scope.anFaqs = false;
+        $scope.anPresentations = false;
+        $scope.anVids = false;
         $scope.anTeam = false;
         $scope.anTeamPics = false;
+        $scope.anAvisory = false;
+        $scope.anAdvisoryPics = false;
         $scope.anSponsors = false;
         $scope.anContact = false;
 
@@ -280,6 +364,8 @@
         var speakersEl = angular.element(document.getElementById("speakers"));
         var faqEl = angular.element(document.getElementById("faq"));
         var teamEl = angular.element(document.getElementById("team"));
+        var presentationsEl = angular.element(document.getElementById("presentations"));
+        var advisoryEl = angular.element(document.getElementById("advisory-board"));
         var sponsorsEl = angular.element(document.getElementById("sponsors"));
         var contactEl = angular.element(document.getElementById("contact"));
 
@@ -303,7 +389,6 @@
              console.log("reached speakers");
              $scope.anSpeakers = true;
            }
-
            if (this.pageYOffset > speakersEl.prop('offsetTop') - 750) {
              console.log("reached speakers");
              $scope.anSpeakerPics = true;
@@ -316,11 +401,23 @@
              console.log("reached faq");
              $scope.anFaqs = true;
            }
+           if (this.pageYOffset > presentationsEl.prop('offsetTop') - 1000) {
+             $scope.anPresentations = true;
+           }
+           if (this.pageYOffset > presentationsEl.prop('offsetTop') - 750) {
+             $scope.anVids = true;
+           }
            if (this.pageYOffset > teamEl.prop('offsetTop')  - 1000) {
              $scope.anTeam = true;
            }
            if (this.pageYOffset > teamEl.prop('offsetTop')  - 750) {
              $scope.anTeamPics = true;
+           }
+           if (this.pageYOffset > advisoryEl.prop('offsetTop')  - 1000) {
+             $scope.anAdvisory = true;
+           }
+           if (this.pageYOffset > advisoryEl.prop('offsetTop')  - 750) {
+             $scope.anAdvisoryPics = true;
            }
            if (this.pageYOffset > sponsorsEl.prop('offsetTop') - 1000) {
              $scope.anSponsors = true;
